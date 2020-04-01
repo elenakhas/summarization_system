f370562c5b08190c2d1ff0a53470e301e2644464
Sophia Chan <ceasings@gmail.com>
2020-04-01 12:55:18 -0700

    D1: added structure

---
+++ Dx.cmd
@@ -0,0 +1,10 @@
+universe = vanilla
+executable = run.sh
+getenv = true
+output = output
+error = myerror.err
+log = mylogfile.log
+notification = complete
+arguments = /corpora/LDC/LDC99T42/RAW/parsed/prd/wsj/14
+transfer_executable = false
+queue
