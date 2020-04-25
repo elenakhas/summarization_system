c5658de21b4a86022e428b70b04c6f8c85b329c0
Erica Gardner <erstgard@uw.edu>
2020-04-25 14:10:01 -0700

    removing extra perl script

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,6 @@
 source activate /home2/schan2/anaconda3/envs/573
 cd src/
 python3 run_pipeline.py
-perl /dropbox/19-20/573/code/ROUGE/ROUGE-1.5.5.pl -e /dropbox/19-20/573/code/ROUGE/data -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d $PWD/rouge_run.xml > $PWD/../results/D2_rouge_scores.out
 
 # do evaluation for training data
 python3 generate_eval_config.py 'D2' 'training' '2009'
