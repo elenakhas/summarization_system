b0ab1fd81ee00688c7f9251cc9b9da2476a4aeaa
Sophia Chan <ceasings@gmail.com>
2020-04-24 23:08:23 -0700

    loading data for devtest works now, sample with 3 topic ids in src/working_files/

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import json
 from collections import defaultdict
 from datetime import datetime
-
+from tqdm import tqdm, trange
 
 DATA_TYPES = (
     "input_data",  
@@ -13,28 +13,89 @@ DATA_TYPES = (
 )
 
 
-
 def get_path_from_docid(doc_id, split, data_store):
-    if "_" in doc_id:
+    if "_" in doc_id:  # Then the document is in ACQUAINT-2
         corpus_dir = data_store["acquaint-2"]
         publication = (doc_id.split("_")[0] + "_" + doc_id.split("_")[1]).lower()
         date = doc_id.split("_")[2].split(".")[0]
         publication_doc = doc_id.split("_")[2].split(".")[1]
         path = corpus_dir + publication + "/" + publication + "_" + date[:-2] + ".xml"
-    else:
-        corpus_dir = data_store["acquaint"]
-        doc_id = doc_id.split(".")[0]
-        publication = doc_id[:3]
-        datestring = doc_id[3:]
-        dt = datetime.strptime(datestring, "%Y%m%d")
-        path = os.path.join(corpus_dir, publication.lower(), str(dt.year), 
-            "{}_{}_ENG".format(datestring, publication))
         return path
-    elif split == "evaltest":
-        return
+    else:  # Then the document is in ACQUAINT
+        corpus_dir = data_store["acquaint"]
+        publication = doc_id[0:3]
+        date = doc_id.split(".")[0][3:]
+        year = date[0:4]
+        if publication == "NYT":
+            path = corpus_dir + publication.lower() + "/" + year + "/" + date + "_" + publication
+        elif publication == "XIE":
+            path = corpus_dir + publication.lower() + "/" + year + "/" + date + "_" + "XIN_ENG"
+        else:
+            path = corpus_dir + publication.lower() + "/" + year + "/" + date + "_" + publication + "_ENG"
+        return path
 
 
-def read_data(xml_filename, split, data_store, test=False):
+def process_acquaint1(path, doc_id):
+    print("processing {} in process_acquaint1".format(path))
+    myfile = open(path, 'r')
+    myfile_data = myfile.read().replace("\n", ' ').strip()
+    document_string = ""
+    doc_soup = BeautifulSoup(myfile_data, 'lxml')
+    results = doc_soup.find_all("docno")
+    for result in results:
+        if result.contents[0].strip() == doc_id:
+            docno = result
+            break
+    
+    doc = docno.parent
+
+    if doc.find("headline"):
+        headline = doc.find("headline").contents[0].strip()
+    else:
+        headline = ""
+    if doc.find("date_time").contents[0].strip():
+        datetime = doc.find("date_time").contents[0].strip()
+    else:
+        datetime = ""
+
+    if doc.find("category"):
+        category = doc.find("category").contents[0].strip()
+    else:
+        category = ""
+    text = doc.find("text").find_all("p")
+    document_string += headline + ". " + datetime + ". " + category + ". "
+
+    for line in text:
+        document_string += line.contents[0].strip() + " "
+    return document_string
+
+
+def process_acquaint2(path, doc_id):
+    print("processing {} in process_acquaint2".format(path))
+    myfile = open(path, 'r')
+    myfile_data = myfile.read().replace("\n", ' ').strip()
+    document_string = ""
+    doc_soup = BeautifulSoup(myfile_data, 'lxml')
+
+    headline = ""
+    dateline = ""
+    if doc_soup.find(id=str(doc_id)).find("headline"):
+        headline = doc_soup.find(id=str(doc_id)).find("headline").contents[0].strip()
+    if doc_soup.find(id=str(doc_id)).find("dateline"):
+        dateline = doc_soup.find(id=str(doc_id)).find("dateline").contents[0].strip()
+    text = doc_soup.find(id=str(doc_id)).find("text").find_all("p")
+
+    # document_string += headline + ". " + dateline + ". "
+    if headline:
+        document_string += headline + ". "
+    if dateline:
+        document_string += dateline + ". "
+    for line in text:
+        document_string += line.contents[0].strip() + " "
+    return document_string    
+
+
+def read_data(xml_filename, split, data_store):
     """
     Args:
         xml_filename (str): TAC documents specification
@@ -60,7 +121,7 @@ def read_data(xml_filename, split, data_store, test=False):
     narrative = [element.contents[0].replace("\t", '').strip() for element in soup.find_all("narrative")]
 
     data = {}
-    for i in range(len(names)):
+    for i in trange(len(names[:3]), desc="topic"):
         name = names[i]
         topic_id = name.get("id")
         if topic_id not in data:
@@ -76,33 +137,13 @@ def read_data(xml_filename, split, data_store, test=False):
                 if str(type(doc)) == "<class 'bs4.element.Tag'>":
                     doc_id = doc["id"]
                     path = get_path_from_docid(doc_id, split, data_store)
-
-                    with open(path) as infile:
-                        data = infile.read()
-                    doc_soup = BeautifulSoup(data, 'lxml')
-
-                    try:
-                        doc_id = doc_soup.find(id=str(doc_id))
-                        # <DOC id="APW_ENG_20041201.0001" type="story" >
-                    except AttributeError:
-                        # <DOCNO> APW19990421.0001 </DOCNO>
-                        # TODO
-                        pass
-
-                    headline = doc_id.find("headline").contents[0].strip()
-                    
-                    if doc_soup.find(id=str(doc_id)).find("dateline"):
-                        dateline = doc_soup.find(id=str(doc_id)).find("dateline").contents[0].strip()
-                    text = doc_soup.find(id=str(doc_id)).find("text").find_all("p")
-                    document += headline + ". " + dateline + ". "
-                    for line in text:
-                        document += line.contents[0].strip() + " "
+                    if "_" in doc_id: # Then the document is in the newer ACQUAINT-2
+                        document = process_acquaint2(path, doc_id)
+                    else:  # Then the document is in the older ACQUAINT-1
+                        document = process_acquaint1(path, doc_id)
 
                     data[topic_id]["docs"][doc_id] = document
-                    
-                    if test:
-                        # Only fetch 1 doc per topic
-                        break
+
     
     print("finished fetching all the data")
     if not os.path.exists(json_path):
