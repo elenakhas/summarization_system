5f975ae6c8c0a3499ec9ab8b6cb70fa3ad3da4cd
Erica Gardner <erstgard@uw.edu>
2020-05-13 22:49:01 -0700

    test40

--- src/generate_summaries.py
+++ src/generate_summaries.py
@@ -42,7 +42,7 @@ def make_summaries(topic_dict, args, data_store):
 
             # ignore short sentences
             sen_length = topic_dict[topic_id][sentence]['length']
-            if sen_length <= 8 or sen_length > 50:
+            if sen_length <= 8 or sen_length > 40:
                 continue
 
             # remove parenthetical expressions
