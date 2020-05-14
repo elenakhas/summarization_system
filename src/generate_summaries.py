5e8c42a26852d838ba175497e646a70dc38baf08
Erica Gardner <erstgard@uw.edu>
2020-05-13 23:46:39 -0700

    starting to modularize make_coherent_summaries

--- src/generate_summaries.py
+++ src/generate_summaries.py
@@ -77,6 +77,84 @@ def make_summaries(topic_dict, args, data_store):
         write_to_file(out_dir, args.run_id, topic_id, sentences)
 
 
+
+
+def make_coherent_summaries(topic_dict, args, data_store):
+    """
+    given a topic dictionary, generates summaries for each topic
+    Args:
+        topic_dict: dictionary of {topic_id: {sentence: {sentence info}}}
+
+    Returns: dictionary of summaries for each topic id. key: topic_id,
+    value: list of summary sentences
+
+    """
+
+    summary_dict = dict()
+    for topic_id in topic_dict.keys():
+        #print(topic_id)
+        summary = []
+        summ_length = 0
+        sorted_keys = sorted(topic_dict[topic_id], key=lambda x: (topic_dict[topic_id][x]['LDAscore']), reverse=True)
+        #print(sorted_keys)
+
+        for sentence in sorted_keys:
+            if summ_length >= 100:
+                break
+
+            # ignore short sentences
+            sen_length = topic_dict[topic_id][sentence]['length']
+            if sen_length <= 8 or sen_length > 50:
+                continue
+
+
+            sentence = apply_heuristics_to_sentence(sentence)
+
+
+
+            tokens = apply_heuristics_to_tokens(word_tokenize(sentence))
+
+
+
+            if summ_length + len(tokens) <= 100:
+                summ_length += len(tokens)
+
+                summary.append(TreebankWordDetokenizer().detokenize(tokens))
+            else:
+                continue # keep going in case we find a shorter sentence to add
+
+        # print("length of summary is {}".format(summ_length))
+        summary_dict[topic_id] = summary
+        # print(summary)
+    # print("length of summary dict is {}".format(len(summary_dict)))
+
+    if args.split == "training":
+        out_dir = data_store["training_outdir"]
+    elif args.split == "devtest":
+        out_dir = os.path.join(data_store["devtest_outdir"], args.deliverable)
+    for topic_id, sentences in summary_dict.items():
+        write_to_file(out_dir, args.run_id, topic_id, sentences)
+
+
+def apply_heuristics_to_sentence(sentence):
+    # remove parenthetical expressions
+    sentence = re.sub("[\(\[].*?[\)\]]", "", sentence)
+
+    return sentence
+
+def apply_heuristics_to_tokens(tokens):
+    # get rid of adverbs
+    pos_tags = [el[1] for el in pos_tag(tokens)]
+
+    adverb_indices = [i for i in range(len(pos_tags)) if pos_tags[i] == 'RB']
+
+    for i in sorted(adverb_indices, reverse=True):
+        tokens.pop(i)
+    return tokens
+
+def score_coherence(sentences):
+    pass
+
 def write_to_file(out_dir, run_id, topic_id, sentences):
     id_part_1 = topic_id[:-1]
     id_part_2 = topic_id[-1]
@@ -108,6 +186,6 @@ if __name__ == "__main__":
     with open(args.candidates) as infile:
         topic_dictionary = json.load(infile)
 
-    summaries_dict = make_summaries(topic_dictionary)
+    summaries_dict = make_coherent_summaries(topic_dictionary) #TODO: change this back if you want to call original function
 
 
