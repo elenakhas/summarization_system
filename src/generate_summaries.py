ecfa070c6e26f48cfa0a88010489e029403f649d
Sophia Chan <ceasings@gmail.com>
2020-05-14 00:34:26 -0700

    change to compiled regexes

--- src/generate_summaries.py
+++ src/generate_summaries.py
@@ -7,14 +7,18 @@ from nltk import pos_tag
 import re
 
 
-def strip_attribution(line, n=4):
-    attribution_pattern = re.compile('[,]([^,\'\"]*?)[.]$')
-    match = attribution_pattern.search(line)
+ATTR_PATTERN = re.compile('[,]([^,\'\"]*?)[.]$')
+PARENS_PATTERN = re.compile("[\(\[].*?[\)\]]")
+QUOTESPACE_PATTERN = re.compile('["] ([A-Za-z0-9])')
+
+
+def strip_attribution(line, n=6):
+    match = ATTR_PATTERN.search(line)
     attribution_words = ("said", "stated", "according")
     if match is not None and any(w in match.group(1) for w in attribution_words):
         if len(word_tokenize(match.group(1))) <= n:
             # print(match.group(1))
-            line = re.sub(attribution_pattern, ".", line)
+            line = ATTR_PATTERN.sub(".", line)
     return line
 
 
@@ -46,7 +50,7 @@ def make_summaries(topic_dict, args, data_store):
                 continue
 
             # remove parenthetical expressions
-            sentence = re.sub("[\(\[].*?[\)\]]", "", sentence)
+            sentence = PARENS_PATTERN.sub("", sentence)
 
             # get rid of adverbs
             tokens = word_tokenize(sentence)
@@ -91,11 +95,14 @@ def write_to_file(out_dir, run_id, topic_id, sentences):
     with open(output_path, "w") as outfile:
         for line in sentences:
             line = line.replace("\\", "").replace(" ,", ", ").replace(" .", ". ").replace("_", " ").replace("  ", " ")
-            line = re.sub('["] ([A-Za-z0-9])', '"\g<1>', line)  # Replace `" The` with `"The` 
+            line = QUOTESPACE_PATTERN.sub('"\g<1>', line)  # Replace `" The` with `"The` 
             line = line.replace(', "', '," ')
             line = line.replace("``", ' "')
 
             line = strip_attribution(line)
+            
+            if not line[0].isupper():
+                line = line[0].upper() + line[1:]
             outfile.write(line + "\n")
 
 
