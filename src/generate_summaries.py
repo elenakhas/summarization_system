a7a2d434a45f8afd31c3b3889f9146e741f8d849
Erica Gardner <erstgard@uw.edu>
2020-05-13 21:05:02 -0700

    adding upper sentence limit 50

--- src/generate_summaries.py
+++ src/generate_summaries.py
@@ -31,7 +31,7 @@ def make_summaries(topic_dict, args, data_store):
 
             # ignore short and long sentences
             sen_length = topic_dict[topic_id][sentence]['length']
-            if sen_length <= 8 or sen_length > 24:
+            if sen_length <= 8 or sen_length > 50:
                 continue
 
             # remove parenthetical expressions
