bc27cba24bf565a8c96a8ab996bd5547b97b2944
Erica Gardner <erstgard@uw.edu>
2020-05-14 20:26:15 -0700

    trying to print similarity scores for sentence pairs

--- src/generate_summaries.py
+++ src/generate_summaries.py
@@ -3,6 +3,8 @@ import os
 from nltk.tokenize import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from nltk import pos_tag
+from itertools import permutations
+from compute_similarity import remove_stopwords, add_lemmas
 import re
 
 
@@ -16,6 +18,94 @@ def strip_attribution(line, n=4):
             line = re.sub(attribution_pattern, ".", line)
     return line
 
+def make_summaries(topic_dict, args, data_store):
+    """
+    given a topic dictionary, generates summaries for each topic
+    version with info ordering
+    Args:
+        topic_dict: dictionary of {topic_id: {sentence: {sentence info}}}
+
+    Returns: dictionary of summaries for each topic id. key: topic_id,
+    value: list of summary sentences
+
+    """
+
+    summary_dict = dict()
+    for topic_id in topic_dict.keys():
+        #print(topic_id)
+        summary = []
+        summ_length = 0
+        sorted_keys = sorted(topic_dict[topic_id], key=lambda x: (topic_dict[topic_id][x]['LDAscore']), reverse=True)
+        #print(sorted_keys)
+
+
+        for sentence in sorted_keys:
+            if summ_length >= 100:
+                break
+
+            # ignore short sentences
+            sen_length = topic_dict[topic_id][sentence]['length']
+            if sen_length <= 8 or sen_length > 50:
+                continue
+
+            sentence = apply_heuristics_to_sentence(sentence)
+
+            tokens = apply_heuristics_to_tokens(word_tokenize(sentence))
+
+
+            if summ_length + len(tokens) <= 100:
+                summ_length += len(tokens)
+
+                summary.append(TreebankWordDetokenizer().detokenize(tokens))
+            else:
+                continue # keep going in case we find a shorter sentence to add
+
+        # print("length of summary is {}".format(summ_length))
+
+        # do information ordering for summary
+        best_summary = score_coherence(summary)
+
+        summary_dict[topic_id] = best_summary
+        # print(summary)
+    # print("length of summary dict is {}".format(len(summary_dict)))
+
+    if args.split == "training":
+        out_dir = data_store["training_outdir"]
+    elif args.split == "devtest":
+        out_dir = os.path.join(data_store["devtest_outdir"], args.deliverable)
+    for topic_id, sentences in summary_dict.items():
+        write_to_file(out_dir, args.run_id, topic_id, sentences)
+
+def score_coherence(summary):
+    perms = permutations(summary, len(summary))
+
+    for count, p in enumerate(perms):
+        print("count is {}".format(count))
+        for i in range(1, len(p)):
+            #print(p[i-1], p[i])
+
+            #calculate_similarity(p[i - 1], p[i])
+
+            s1 = spacy_lm(p[i-1].lower())
+            s2 = spacy_lm(p[i].lower())
+            s1_no_stop = remove_stopwords(s1, spacy_lm)
+            s2_no_stop = remove_stopwords(s2, spacy_lm)
+            s1_processed = add_lemmas(s1_no_stop, spacy_lm)
+            s2_processed = add_lemmas(s2_no_stop, spacy_lm)
+
+            print(s1_processed.similarity(s2_processed))
+
+
+def calculate_similarity(s1, s2):
+    """
+    calculates similarity score for a sentence pair
+    Args:
+        s1: first sentence
+        s2: second sentence
+
+    Returns: cosine similarity score
+
+    """
 
 # def make_summaries(topic_dict, args, data_store):
 #     """
@@ -45,18 +135,11 @@ def strip_attribution(line, n=4):
 #             if sen_length <= 8 or sen_length > 50:
 #                 continue
 #
-#             # remove parenthetical expressions
-#             sentence = re.sub("[\(\[].*?[\)\]]", "", sentence)
+#             sentence = apply_heuristics_to_sentence(sentence)
 #
-#             # get rid of adverbs
-#             tokens = word_tokenize(sentence)
-#             pos_tags = [el[1] for el in pos_tag(tokens)]
+#             tokens = apply_heuristics_to_tokens(word_tokenize(sentence))
 #
-#             adverb_indices = [i for i in range(len(pos_tags)) if pos_tags[i] == 'RB']
-#             #print(adverb_indices)
-#             for i in sorted(adverb_indices, reverse=True):
-#                 tokens.pop(i)
-#             #print(tokens, pos_tags)
+#
 #             if summ_length + len(tokens) <= 100:
 #                 summ_length += len(tokens)
 #
@@ -75,60 +158,6 @@ def strip_attribution(line, n=4):
 #         out_dir = os.path.join(data_store["devtest_outdir"], args.deliverable)
 #     for topic_id, sentences in summary_dict.items():
 #         write_to_file(out_dir, args.run_id, topic_id, sentences)
-
-
-
-def make_summaries(topic_dict, args, data_store):
-    """
-    given a topic dictionary, generates summaries for each topic
-    Args:
-        topic_dict: dictionary of {topic_id: {sentence: {sentence info}}}
-
-    Returns: dictionary of summaries for each topic id. key: topic_id,
-    value: list of summary sentences
-
-    """
-
-    summary_dict = dict()
-    for topic_id in topic_dict.keys():
-        #print(topic_id)
-        summary = []
-        summ_length = 0
-        sorted_keys = sorted(topic_dict[topic_id], key=lambda x: (topic_dict[topic_id][x]['LDAscore']), reverse=True)
-        #print(sorted_keys)
-
-        for sentence in sorted_keys:
-            if summ_length >= 100:
-                break
-
-            # ignore short sentences
-            sen_length = topic_dict[topic_id][sentence]['length']
-            if sen_length <= 8 or sen_length > 50:
-                continue
-
-            sentence = apply_heuristics_to_sentence(sentence)
-
-            tokens = apply_heuristics_to_tokens(word_tokenize(sentence))
-
-
-            if summ_length + len(tokens) <= 100:
-                summ_length += len(tokens)
-
-                summary.append(TreebankWordDetokenizer().detokenize(tokens))
-            else:
-                continue # keep going in case we find a shorter sentence to add
-
-        # print("length of summary is {}".format(summ_length))
-        summary_dict[topic_id] = summary
-        # print(summary)
-    # print("length of summary dict is {}".format(len(summary_dict)))
-
-    if args.split == "training":
-        out_dir = data_store["training_outdir"]
-    elif args.split == "devtest":
-        out_dir = os.path.join(data_store["devtest_outdir"], args.deliverable)
-    for topic_id, sentences in summary_dict.items():
-        write_to_file(out_dir, args.run_id, topic_id, sentences)
 
 
 def apply_heuristics_to_sentence(sentence):
@@ -181,8 +210,6 @@ def apply_heuristics_to_tokens(tokens):
 
     return tokens
 
-def score_coherence(sentences):
-    pass
 
 def write_to_file(out_dir, run_id, topic_id, sentences):
     id_part_1 = topic_id[:-1]
